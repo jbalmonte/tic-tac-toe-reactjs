b240f5e30203e74b3c32dec51e82b8ee01e65529
jbalmonte <jbalmonte45@gmail.com>
2021-09-22 16:31:22 +0800

    UI fix

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function TicTacToe() {
 
   const handleClick = e => {
-    const target = e.target.closest('td')
-    if (target && !target.classList.contains('td-active')) {
-      target.className = 'td-active'
+    const target = e.target.closest('button')
+    if (target) {
+      target.className = 'element-active'
       target.textContent = "X"
     }
   }
@@ -13,26 +13,32 @@ export default function TicTacToe() {
   return (
     <main>
       <h1>Player X, it's your turn!</h1>
-      <table cellSpacing={10} onClick={handleClick}>
+      <table onClick={handleClick}>
         <tbody>
           <tr>
-            <td className="td"><div className="dot" /></td>
-            <td className="td"><div className="dot" /></td>
-            <td className="td"><div className="dot" /></td>
+            <td><button className="element" >.</button></td>
+            <td><button className="element" >.</button></td>
+            <td><button className="element" >.</button></td>
           </tr>
 
           <tr>
-            <td className="td"><div className="dot" /></td>
-            <td className="td"><div className="dot" /></td>
-            <td className="td"><div className="dot" /></td>
+            <td><button className="element" >.</button></td>
+            <td><button className="element" >.</button></td>
+            <td><button className="element" >.</button></td>
           </tr>
           <tr>
-            <td className="td"><div className="dot" /></td>
-            <td className="td"><div className="dot" /></td>
-            <td className="td"><div className="dot" /></td>
+            <td><button className="element" >.</button></td>
+            <td><button className="element" >.</button></td>
+            <td><button className="element" >.</button></td>
           </tr>
         </tbody>
       </table>
+      <div className="state-container">
+        <button className="btn-state" onClick={e => e.target.className = "btn-state-active"}>0</button>
+        <button className="btn-state" onClick={e => e.target.className = "btn-state-active"}>1</button>
+        <button className="btn-state" onClick={e => e.target.className = "btn-state-active"}>2</button>
+      </div>
+      <button className="btn-restart">Restart</button>
     </main>
   );
 }
